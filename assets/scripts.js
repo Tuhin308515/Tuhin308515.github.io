86bce8307d56758e972efafcc0d29d9b485accb8
c1943022 <shadmo@cardiff.ac.uk>
2025-02-12 11:51:00 +0000

    Reverting changes

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,19 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
     ];
     let currentVideoIndex = 0;
 
-    // Function to change video
-    function changeVideo() {
-        currentVideoIndex = (currentVideoIndex + 1) % sources.length;
-        video.src = sources[currentVideoIndex];
-        video.load(); // Ensure the new video source is loaded
-        video.play();
-    }
-
-    // Set initial video
     video.src = sources[currentVideoIndex];
-    video.load();
     video.play();
 
-    // Listen for video end event
-    video.addEventListener("ended", changeVideo);
+    video.addEventListener("ended", () => {
+        currentVideoIndex = (currentVideoIndex + 1) % sources.length;
+        video.src = sources[currentVideoIndex];
+        video.play();
+    });
 });
